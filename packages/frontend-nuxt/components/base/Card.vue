eb5926cfd4fa2c8e4cc5720ad481c39a4a8dbdd3
ayoshii27 <dayoshii27@gmail.com>
2025-07-26 19:16:21 +0900

    feat: 🎸 cardにタイトルを渡せるようにする

--- packages/frontend-nuxt/components/base/Card.vue
+++ packages/frontend-nuxt/components/base/Card.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
+defineProps<{
+  title?: string;
+}>();
 </script>
 
 <template>
   <section class="base-card">
-    <slot />
+    <h2
+      v-if="title"
+      class="title"
+    >
+      {{ title }}
+    </h2>
+    <div class="contents">
+      <slot />
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 .base-card {
-  padding: 16px;
   background: #fff;
-  box-shadow: 2px 2px 7px rgba(0, 0, 0, .2);
+  box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
+}
+.title {
+  padding: 16px 16px 8px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.contents {
+  padding: 16px;
 }
 </style>
