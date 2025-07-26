73cae09267cff17e8f3b753b1cf0d4a6f9eaa221
ayoshii27 <dayoshii27@gmail.com>
2025-07-26 21:09:59 +0900

    feat: 🎸 カスタム集計項目テーブルを表示する（API繋ぎこみ）

--- packages/frontend-nuxt/pages/countings/monthly/[yearMonth].vue
+++ packages/frontend-nuxt/pages/countings/monthly/[yearMonth].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import dayjs from "dayjs";
+import type { CustomCountingItem } from "~/types/custom-counting-item.type";
 
 const route = useRoute();
 const yearMonth = route.params.yearMonth as string;
@@ -25,6 +26,13 @@ const navigateToNextMonth = () => {
 const navigateToExpendsPage = () => {
   navigateTo("/expends/monthly/" + yearMonth);
 };
+
+const { data: customCountingItems, execute: fetchCustomCountingItems } =
+  useAsyncApiFetchData<CustomCountingItem[]>("new-custom-counting-items", {
+    yearMonth,
+  });
+
+await fetchCustomCountingItems();
 </script>
 
 <template>
@@ -41,9 +49,30 @@ const navigateToExpendsPage = () => {
         >{{ toExpendsPageButtonLabel }}</BaseButton
       >
     </div>
-    <base-card>
-      TODO: 集計テーブル
-    </base-card>
+    <div class="counting-list-container">
+      <base-card title="カスタム集計一覧">
+        <base-table>
+          <thead>
+            <tr>
+              <th>集計項目名</th>
+              <th>金額</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="customCountingItem in customCountingItems"
+              :key="customCountingItem.id"
+            >
+              <td>{{ customCountingItem.name }}</td>
+              <td>{{ formatPrice(customCountingItem.price) }}</td>
+            </tr>
+          </tbody>
+        </base-table>
+      </base-card>
+      <base-card title="カテゴリー別集計一覧">
+        TODO: カテゴリー別集計項目
+      </base-card>
+    </div>
   </main>
 </template>
 
@@ -60,5 +89,11 @@ const navigateToExpendsPage = () => {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.counting-list-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 8px;
 }
 </style>
---
+++ packages/frontend-nuxt/types/custom-counting-item.type.ts
@@ -0,0 +1,8 @@
+export type CustomCountingItem = {
+  /** カスタム集計項目ID */
+  id: number;
+  /** カスタム集計項目名 */
+  name: string;
+  /** カスタム集計項目の集計金額 */
+  price: number;
+};
