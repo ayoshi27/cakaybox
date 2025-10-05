ca50d1496b83f2b499061e8e1ce24adfd8b85377
ayoshii27 <dayoshii27@gmail.com>
2025-10-05 12:53:40 +0900

    feat: 🎸 集計画面のスケルトンを実装する

--- packages/frontend-nuxt/pages/countings/monthly/[yearMonth].vue
+++ packages/frontend-nuxt/pages/countings/monthly/[yearMonth].vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
 import dayjs from "dayjs";
-import type {
-  CustomCountingItem,
-  MonthlyCalculatedPriceForCategory,
-} from "~/types/custom-counting-item.type";
 
 const route = useRoute();
 const yearMonth = route.params.yearMonth as string;
@@ -29,26 +25,6 @@ const navigateToNextMonth = () => {
 const navigateToExpendsPage = () => {
   navigateTo("/expends/monthly/" + yearMonth);
 };
-
-const { data: customCountingItems, execute: fetchCustomCountingItems } =
-  useAsyncApiFetchData<CustomCountingItem[]>("new-custom-counting-items", {
-    yearMonth,
-  });
-
-const {
-  data: monthlyCalculatedPriceForCategory,
-  execute: fetchMonthlyCalculatedPriceForCategory,
-} = useAsyncApiFetchData<MonthlyCalculatedPriceForCategory[]>(
-  "expends/monthly-calculated",
-  {
-    yearMonth,
-  }
-);
-
-await Promise.all([
-  fetchCustomCountingItems(),
-  fetchMonthlyCalculatedPriceForCategory(),
-]);
 </script>
 
 <template>
@@ -65,55 +41,17 @@ await Promise.all([
         >{{ toExpendsPageButtonLabel }}</BaseButton
       >
     </div>
-    <div class="counting-list-container">
-      <base-card title="カスタム集計一覧">
-        <base-table>
-          <thead>
-            <tr>
-              <th>集計項目名</th>
-              <th>金額</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="customCountingItem in customCountingItems"
-              :key="customCountingItem.id"
-            >
-              <td>{{ customCountingItem.name }}</td>
-              <td>{{ formatPrice(customCountingItem.price) }}</td>
-            </tr>
-          </tbody>
-        </base-table>
-      </base-card>
-      <base-card title="カテゴリー別集計一覧">
-        <base-table>
-          <thead>
-            <tr>
-              <th>カテゴリー名</th>
-              <th>金額</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="category in monthlyCalculatedPriceForCategory"
-              :key="category.id"
-            >
-              <td>
-                <div class="category-cell-content">
-                  <Icon
-                    name="mdi:local-offer"
-                    :style="categoryIconStyle(category.categoryColorCode)"
-                    class="category-icon"
-                  />
-                  {{ category.categoryName }}
-                </div>
-              </td>
-              <td>{{ formatPrice(category.price) }}</td>
-            </tr>
-          </tbody>
-        </base-table>
-      </base-card>
-    </div>
+    <Suspense>
+      <template #default>
+        <counting-monthly-summary
+          :yearMonth="yearMonth"
+          :key="yearMonth"
+        />
+      </template>
+      <template #fallback>
+        <counting-monthly-summary-skelton />
+      </template>
+    </Suspense>
   </main>
 </template>
 
@@ -122,19 +60,9 @@ await Promise.all([
   padding: 16px;
 }
 
-.base-card {
-  margin-top: 16px;
-}
-
 .page-operation-container {
   display: flex;
   justify-content: space-between;
   align-items: center;
-}
-
-.counting-list-container {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 8px;
 }
 </style>
